50de50e592616f14f1d7a08b3a83183e2509d572
Scighost <scighost@outlook.com>
2022-08-13 22:28:34 +0800

    成就

---
+++ Xunkong.GenshinData/Material/MaterialType.cs
@@ -0,0 +1,135 @@
+﻿namespace Xunkong.GenshinData.Material;
+
+
+/// <summary>
+/// 材料类型（全大写的变量名表示尚未使用，以后会更改变量名）
+/// </summary>
+public abstract class MaterialType
+{
+
+    /// <summary>
+    /// 虚拟物品，比如经验
+    /// </summary>
+    public const string Virtual = "";
+
+
+    public const string MATERIAL_FAKE_ABSORBATE = "MATERIAL_FAKE_ABSORBATE";
+
+
+    public const string MATERIAL_ADSORBATE = "MATERIAL_ADSORBATE";
+
+
+    public const string MATERIAL_CONSUME = "MATERIAL_CONSUME";
+
+
+    public const string MATERIAL_TALENT = "MATERIAL_TALENT";
+
+    /// <summary>
+    /// 角色（角色也是材料）
+    /// </summary>
+
+    public const string Avatar = "MATERIAL_AVATAR";
+
+
+    public const string MATERIAL_CHEST = "MATERIAL_CHEST";
+
+
+    public const string MATERIAL_NOTICE_ADD_HP = "MATERIAL_NOTICE_ADD_HP";
+
+
+    public const string MATERIAL_EXCHANGE = "MATERIAL_EXCHANGE";
+
+
+    public const string MATERIAL_WOOD = "MATERIAL_WOOD";
+
+
+    public const string MATERIAL_QUEST = "MATERIAL_QUEST";
+
+
+    public const string MATERIAL_CRICKET = "MATERIAL_CRICKET";
+
+
+    public const string MATERIAL_WIDGET = "MATERIAL_WIDGET";
+
+
+    public const string MATERIAL_ELEM_CRYSTAL = "MATERIAL_ELEM_CRYSTAL";
+
+
+    public const string MATERIAL_SPICE_FOOD = "MATERIAL_SPICE_FOOD";
+
+
+    public const string MATERIAL_ACTIVITY_GEAR = "MATERIAL_ACTIVITY_GEAR";
+
+
+    public const string MATERIAL_ACTIVITY_ROBOT = "MATERIAL_ACTIVITY_ROBOT";
+
+
+    public const string MATERIAL_ACTIVITY_JIGSAW = "MATERIAL_ACTIVITY_JIGSAW";
+
+
+    public const string MATERIAL_FOOD = "MATERIAL_FOOD";
+
+
+    public const string MATERIAL_EXP_FRUIT = "MATERIAL_EXP_FRUIT";
+
+
+    public const string MATERIAL_WEAPON_EXP_STONE = "MATERIAL_WEAPON_EXP_STONE";
+
+
+    public const string MATERIAL_AVATAR_MATERIAL = "MATERIAL_AVATAR_MATERIAL";
+
+
+    public const string MATERIAL_RELIQUARY_MATERIAL = "MATERIAL_RELIQUARY_MATERIAL";
+
+
+    public const string MATERIAL_CONSUME_BATCH_USE = "MATERIAL_CONSUME_BATCH_USE";
+
+
+    public const string MATERIAL_FISH_BAIT = "MATERIAL_FISH_BAIT";
+
+
+    public const string MATERIAL_CHEST_BATCH_USE = "MATERIAL_CHEST_BATCH_USE";
+
+
+    public const string MATERIAL_SELECTABLE_CHEST = "MATERIAL_SELECTABLE_CHEST";
+
+
+    public const string MATERIAL_HOME_SEED = "MATERIAL_HOME_SEED";
+
+
+    public const string MATERIAL_FLYCLOAK = "MATERIAL_FLYCLOAK";
+
+
+    public const string MATERIAL_BGM = "MATERIAL_BGM";
+
+
+    public const string MATERIAL_SEA_LAMP = "MATERIAL_SEA_LAMP";
+
+
+    public const string MATERIAL_CHANNELLER_SLAB_BUFF = "MATERIAL_CHANNELLER_SLAB_BUFF";
+
+
+    public const string MATERIAL_FISH_ROD = "MATERIAL_FISH_ROD";
+
+
+    /// <summary>
+    /// 名片
+    /// </summary>
+    public const string NameCard = "MATERIAL_NAMECARD";
+
+
+    public const string MATERIAL_FIREWORKS = "MATERIAL_FIREWORKS";
+
+    /// <summary>
+    /// 服装
+    /// </summary>
+    public const string Costume = "MATERIAL_COSTUME";
+
+
+    public const string MATERIAL_FURNITURE_SUITE_FORMULA = "MATERIAL_FURNITURE_SUITE_FORMULA";
+
+
+    public const string MATERIAL_FURNITURE_FORMULA = "MATERIAL_FURNITURE_FORMULA";
+
+
+}
