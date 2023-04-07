0c0144b193b8b76a5098071f382cc6ace95e6dbc
Scighost <scighost@outlook.com>
2023-04-07 11:28:15 +0800

    更新文本模型

--- Xunkong.GenshinData/Text/TextMap.cs
+++ Xunkong.GenshinData/Text/TextMap.cs
@@ -2,48 +2,20 @@
 
 public class TextMap
 {
-    public long Id { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
 
 
-    public TextMapType Type { get; set; }
+    [JsonPropertyName("version")]
+    public string Version { get; set; }
 
 
-    public string? zh_cn { get; set; }
+    [JsonPropertyName("update_time")]
+    public DateTimeOffset UpdateTime { get; set; }
 
 
-    public string? zh_tw { get; set; }
-
-
-    public string? de_de { get; set; }
-
-
-    public string? en_us { get; set; }
-
-
-    public string? es_es { get; set; }
-
-
-    public string? fr_fr { get; set; }
-
-
-    public string? id_id { get; set; }
-
-
-    public string? ja_jp { get; set; }
-
-
-    public string? ko_kr { get; set; }
-
-
-    public string? pt_pt { get; set; }
-
-
-    public string? ru_ru { get; set; }
-
-
-    public string? th_th { get; set; }
-
-
-    public string? vi_vn { get; set; }
+    [JsonPropertyName("list")]
+    public List<TextMapItem> List { get; set; }
 
 }
